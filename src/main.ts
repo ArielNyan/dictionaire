3c99e7b4e2b56bc84854d82c5d8be638bee841aa
ArielNyan <arthur.rigotti@gmail.com>
2024-10-01 12:21:15 -0300

    adição de um sistma de container para facilitar estilização

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import $ from "jquery"
 /* declaração de coisas uteis talvez*/
-//TODO Fazer funções pra fazilitar designar tags para os componentes
 
 
 /* declaração de componentes */
@@ -14,22 +13,28 @@ function phonetics(phonetics: Array<number>){
   let content = $('<div>').attr('id', 'phonetics')
   let lengh = phonetics.length - 1
   for(var i = 0; i <= lengh; i++){
+
+    let container = $("<div>").attr({
+      id: `phonetic_${i}`,
+      class: "phonetic"
+    })
     for(const [key, value] of Object.entries((phonetics[i]))){
       if(key == "license"){      
-        const licenseName = $('<p>').attr(`id`, `license_${value.name}`).text(`${value.name}`)       
-        const licenseUrl = $('<a>').attr({id: `licence_${value.url}`, href: `${value.url}`}).text(`${value.url}`)
-        content.append(licenseName, licenseUrl)
-      }else if(key == "audio"){
+        // const licenseName = $('<p>').attr(`id`, `license_${value.name}`).text(`${value.name}`)       
+        // const licenseUrl = $('<a>').attr({id: `licence_${value.url}`, href: `${value.url}`}).text(`${value.url}`)
+        // content.append(licenseName, licenseUrl)
+      }else if(key == "audio" && value !== ""){
         const audioTag = $("<audio controls>").attr("class", "audioTag")
         const audioUrl = $('<source>').attr({class: "audio", src: `${value}`, type: "audio/mp3"})
         audioTag.append(audioUrl)
-        content.append(audioTag, $("<br>"))
+        container.append(audioTag, $("<br>"))
       }else if(key == "sourceUrl"){/*Do Nothing*/}
       else{      
-        const innerValue = $('<p>').attr({id: `phonetic_${value}`, class: 'phonetic'}).text(`${value}`)
-        content.append(innerValue)
+        const innerValue = $('<p>').attr({id: `phonetic_${i}`, class: 'phonetic'}).text(`${value}`)
+        container.append(innerValue)
       }
     }
+    container.appendTo(content)
   }
   return content.appendTo(app)
 }
@@ -41,6 +46,10 @@ function meanings(meanings: Array<number>){
   })
   let length = meanings.length - 1
   for(let i = 0; i <= length; i++){
+    let container = $("<div>").attr({
+      id: `container_${i}`,
+      class: "container"
+    })
     for(const [key, value] of Object.entries((meanings[i]))){
       // if(key == "definitions"){
       //   value.forEach((e: Object) => {
@@ -52,13 +61,14 @@ function meanings(meanings: Array<number>){
       //   content.append(innerValue)
       // }
       //ESSA MERDA VAI SER UM INFERNO SE EU PRECISAR VOLTAR AQUI
+      //eu precisei voltar aqui algumas vezes...
       switch (key) {
         case "partOfSpeech":
           const partSpeech = $("<p>").attr({
             id: `partSpeech_${i}`,
             class: "partSpeech"
           }).text(`${value}`)
-          partSpeech.appendTo(content)
+          partSpeech.appendTo(container)
           break;
         case "definitions": 
         value.forEach((e: Object) => {
@@ -70,28 +80,28 @@ function meanings(meanings: Array<number>){
                     id: `def_${i}`,
                     class:"definition"
                   }).text(`${v}`)
-                  def.appendTo(content)
+                  def.appendTo(container)
                 break;
                 case "synonyms":
                   const syn = $("<p>").attr({
                     id: `syn_${i}`,
                     class: "synonyms"
                   }).text(`${v}`)
-                  syn.appendTo(content)
+                  syn.appendTo(container)
                 break;
                 case "antonyms":
                   const antonyms = $("<p>").attr({
                     id: `anto_${i}`,
                     class: "antonyms"
                   }).text(`${v}`)
-                  antonyms.appendTo(content)
+                  antonyms.appendTo(container)
                 break;
                 case "example":
                   const ex = $("<p>").attr({
                     id: `ex_${i}`,
                     class: `example`
                   }).text(`${v}`)
-                  ex.appendTo(content)
+                  ex.appendTo(container)
                 break;
               }
             }
@@ -102,7 +112,7 @@ function meanings(meanings: Array<number>){
               id: `synonyms_${i}`,
               class: "synonyms"
             }).text(`${value}`)
-            syn.appendTo(content)
+            syn.appendTo(container)
           break;
           case "antonyms":
 
@@ -110,11 +120,12 @@ function meanings(meanings: Array<number>){
               id: `anto_${i}`,
               class: "antonyms"
             }).text(`${value}`)
-            antonyms.appendTo(content)
+            antonyms.appendTo(container)
           break;
 
       }
     }
+    container.appendTo(content)
   }
   return content.appendTo(app)
 }
@@ -124,7 +135,7 @@ const app = $('#app')
 
 
 $.getJSON('https://api.dictionaryapi.dev/api/v2/entries/en/hello', (res: any) => {
-  //app.append(JSON.stringify(res[0].meanings))
+  // app.append(JSON.stringify(res[0].phonetics))
   word(res[0].word)
   phonetics(res[0].phonetics)
   meanings(res[0].meanings)
